aa1931efaca74766a303cf25bcf3f2c804490ee6
Bhakti Sojitra <sojitrabhakti03@gmail.com>
2025-01-19 11:22:34 +0530

    Update OrderDetailModel.cs

--- Models/OrderDetailModel.cs
+++ Models/OrderDetailModel.cs
@@ -2,7 +2,7 @@
 {
     public class OrderDetailModel
     {
-        public int OrderDetailID { get; set; }
+        public int? OrderDetailID { get; set; }
         public int OrderID { get; set; }
         public int ProductID { get; set; }
         public int Quantity { get; set; }
